44b09c7c3bdbb0aedaae3fd77496d35459bfa97f
曹晶 <cao156@outlook.com>
2024-10-16 16:11:03 +0800

    feat(media): add getTempFile api (#801)
    add getTempFile api

--- work/material/media.go
+++ work/material/media.go
@@ -14,6 +14,8 @@ const (
 	uploadTempFile = "https://qyapi.weixin.qq.com/cgi-bin/media/upload?access_token=%s&type=%s"
 	// uploadAttachment 上传附件资源
 	uploadAttachment = "https://qyapi.weixin.qq.com/cgi-bin/media/upload_attachment?access_token=%s&media_type=%s&attachment_type=%d"
+	// getTempFile 获取临时素材
+	getTempFile = "https://qyapi.weixin.qq.com/cgi-bin/media/get?access_token=%s&media_id=%s"
 )
 
 // UploadImgResponse 上传图片响应
@@ -147,4 +149,22 @@ func (r *Client) UploadAttachmentFromReader(filename, mediaType string, reader i
 	result := &UploadAttachmentResponse{}
 	err = util.DecodeWithError(response, result, "UploadAttachment")
 	return result, err
+}
+
+// GetTempFile 获取临时素材
+// @see https://developer.work.weixin.qq.com/document/path/90254
+func (r *Client) GetTempFile(mediaID string) ([]byte, error) {
+	var (
+		accessToken string
+		err         error
+	)
+	if accessToken, err = r.GetAccessToken(); err != nil {
+		return nil, err
+	}
+	url := fmt.Sprintf(getTempFile, accessToken, mediaID)
+	response, err := util.HTTPGet(url)
+	if err != nil {
+		return nil, err
+	}
+	return response, nil
 }
